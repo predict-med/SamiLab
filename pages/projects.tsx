d2bb124c1944fcab823c9c0889322e54a0007a65
FL33TW00D <fleetwoodpersonal@gmail.com>
2021-08-17 21:59:58 +0100

    projects card

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,4 +1,6 @@
 import Layout from "../components/layout";
+import Image from "next/image";
+import connectivity from "../public/projects/connectivity.png"
 
 export default function Projects() {
   return (
@@ -11,11 +13,12 @@ export default function Projects() {
           Our mission is to address the personalised medicine gap in dementia
           research.
         </div>
-        <div className="w-full max-w-6xl border border-gray-100 mx-auto"></div>
+        <div className="w-full max-w-4xl border border-gray-100 mx-auto"></div>
         <div className="flex flex-col md:flex-row justify-center flex-wrap gap-3 mt-10  ">
           <div>
-            <div className="bg-white max-w-xs shadow-lg mx-auto border-b-4 border-indigo-500 rounded-2xl overflow-hidden  hover:shadow-2xl transition duration-500 transform hover:scale-105 cursor-pointer">
-              <div className="bg-indigo-500  flex h-32  items-center">
+            <div className="bg-white max-w-xs shadow-lg mx-auto border-b-4 border-green-200 rounded-2xl overflow-hidden  hover:shadow-xl transition duration-500 transform hover:scale-105 cursor-pointer">
+                <div className="flex h-32  items-center"> 
+                    <Image src={connectivity} /> 
               </div>
               <p className="py-6 px-6 text-lg tracking-wide text-center">
                 Poster
@@ -23,7 +26,7 @@ export default function Projects() {
               <div className="flex justify-center px-5 mb-2 text-sm ">
                 <button
                   type="button"
-                  className="border border-indigo-500 text-indigo-500 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-green-600 focus:outline-none focus:shadow-outline"
+                  className="border border-green-200 text-black rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white hover:bg-green-600 focus:outline-none focus:shadow-outline"
                 >
                   Details
                 </button>
